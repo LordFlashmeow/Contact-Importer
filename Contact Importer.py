d3d43a1f866567f1cbe50a7a2b0beaa37682c14a
LordFlashmeow <dpeters19@bayschoolsf.org>
2016-04-21 13:08:57 -0700

    Update Contact Importer.py

--- Contact Importer.py
+++ Contact Importer.py
@@ -1,9 +1,9 @@
-outputType = input("Enter the type of output file. (csv/vcl) ")
-if outputType == "csv":
+outputType = input("Enter the type of output file. (c/v) ")
+if outputType == "c":
     fileName = input("Enter the name of the file to work with ") + ".csv"
 
 
-while outputType == "csv":
+while outputType == "c":
     name = input("Enter the person's name \n")
     if name == "q":
         break
@@ -15,12 +15,12 @@ while outputType == "csv":
     print("Submitted \n")
 
 
-while outputType == "vcl":
+while outputType == "v":
     name = input("Enter the person's name \n")
     if name == "q":
         break
     email = input("Enter the person;s name \n")
-    combined = "BEGIN:VCARD\nVERSION:4.0\n" + "FN:" + name + "\n" + "EMAIL:" + email + "END:VCARD"
+    combined = "BEGIN:VCARD\nVERSION:4.0\n" + "FN:" + name + "\n" + "EMAIL:" + email +"\n" + "END:VCARD"
     tempname = name + ".vcl"
     people = open(tempname, 'a')
     people.write(combined)
@@ -29,21 +29,3 @@ while outputType == "vcl":
     tempname = None
 
 done = input("Press enter to quit")
-
-
-"""BEGIN:VCARD
-VERSION:4.0
-N:Gump;Forrest;;;
-FN:Forrest Gump
-ORG:Bubba Gump Shrimp Co.
-TITLE:Shrimp Man
-PHOTO;MEDIATYPE=image/gif:http://www.example.com/dir_photos/my_photo.gif
-TEL;TYPE=work,voice;VALUE=uri:tel:+11115551212
-TEL;TYPE=home,voice;VALUE=uri:tel:+14045551212
-ADR;TYPE=work;LABEL="100 Waters Edge\nBaytown, LA 30314\nUnited States of A
- merica":;;100 Waters Edge;Baytown;LA;30314;United States of America
-ADR;TYPE=home;LABEL="42 Plantation St.\nBaytown, LA 30314\nUnited States of
- America":;;42 Plantation St.;Baytown;LA;30314;United States of America
-EMAIL:forrestgump@example.com
-REV:20080424T195243Z
-END:VCARD"""
