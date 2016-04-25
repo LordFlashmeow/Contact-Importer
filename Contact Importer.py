e6a03446d2917b736eab0f632a14eea6e1c42b35
Dash Peters <dpeters19@bayschoolsf.org>
2016-04-25 10:04:36 -0700

    Add first version of Auto-Selecting file format

--- Contact Importer.py
+++ Contact Importer.py
@@ -1,31 +1,55 @@
-outputType = input("Enter the type of output file. (c/v) ")
-if outputType == "c":
-    fileName = input("Enter the name of the file to work with ") + ".csv"
+outputAuto = None
+outputType = None
+def autoOutput():
+    global outputAuto
+    global outputType
+    outputAuto = input("Do you want to automatically select the file type? (y/n) ")
+    if outputAuto == "y":
+        outputAuto = True
+    elif outputAuto == "n":
+        outputAuto = False
+        outputType = input("Enter the type of output file. (c/v) ")
+        if outputType == "c":
+            fileName = input("Enter the name of the file to work with ") + ".csv"
+    else:
+        print("Your input was not recognised. Please try again")
+        autoOutput()
 
 
-while outputType == "c":
+def CSV():
+    global outputType
+    global outputAuto
+    while outputType == "c" and outputAuto is False:
+        name = input("Enter the person's name \n")
+        if name == "q":
+            break
+        email = input("Enter the person's email \n")
+        combined = name + "," + email + "\n"
+        people = open(fileName, 'a')
+        people.write(combined)
+        people.close()
+        print("Submitted \n")
+def vCard():
+    while outputType == "v" and outputAuto is False:
+        name = input("Enter the person's name \n")
+        if name == "q":
+            break
+        email = input("Enter the person's name \n")
+        combined = "BEGIN:VCARD" \
+                   "\nVERSION:4.0\n" + "FN:" + name + "\n" + "EMAIL:" + email + "\n" + "END:VCARD"
+        tempname = name + ".vcl"
+        people = open(tempname, 'a')
+        people.write(combined)
+        people.close()
+        print("Submitted \n")
+        tempname = None
+
+while outputAuto is True:
     name = input("Enter the person's name \n")
-    if name == "q":
-        break
+        if name == "q":
+            break
     email = input("Enter the person's email \n")
-    combined = name + "," + email + "\n"
-    people = open(fileName, 'a')
-    people.write(combined)
-    people.close()
-    print("Submitted \n")
 
 
-while outputType == "v":
-    name = input("Enter the person's name \n")
-    if name == "q":
-        break
-    email = input("Enter the person;s name \n")
-    combined = "BEGIN:VCARD\nVERSION:4.0\n" + "FN:" + name + "\n" + "EMAIL:" + email +"\n" + "END:VCARD"
-    tempname = name + ".vcl"
-    people = open(tempname, 'a')
-    people.write(combined)
-    people.close()
-    print("Submitted \n")
-    tempname = None
 
 done = input("Press enter to quit")
